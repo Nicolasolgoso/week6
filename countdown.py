d9fc9e2b556c3a51d27d0c855cd8b189c89b6c33
Nicolasolgoso <145667874+Nicolasolgoso@users.noreply.github.com>
2023-10-26 20:14:20 +0200

    Create countdown.py

---
+++ countdown.py
@@ -0,0 +1,11 @@
+number = 10
+
+while number >= 0:
+    if number >= 3 and number <= 7:
+        if number == 3:
+            print("...")
+    else:
+        print(number)
+    number -= 1
+
+print("BOOM")
